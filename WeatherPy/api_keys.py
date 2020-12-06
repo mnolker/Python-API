da6ded350cd66c257fa35014dc3188d851866ddf
Melanie Nolker <melanie.nolker@kiewit.com>
2020-12-06 11:46:27 -0600

    ignore api_keys.py

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "5c59bef5121206c3cd59968bee0a49fd"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyC5652TBqbEvvmzbo9St2J0ARm58rcKVa4"
